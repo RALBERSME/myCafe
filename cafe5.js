24168c9654c4485e6a172ff652c3c935130f1d47
RALBERSME <149826808+RALBERSME@users.noreply.github.com>
2024-10-05 19:48:07 +0200

    coffee varieties added

---
+++ cafe5.js
@@ -0,0 +1,32 @@
+const arabica = document.getElementById("arabica");
+function openArabica() {
+  arabica.classList.remove("hide");
+}
+
+function closeArabica() {
+  arabica.classList.add("hide");
+}
+const robusta = document.getElementById("robusta");
+function openRobusta() {
+  robusta.classList.remove("hide");
+}
+
+function closeRobusta() {
+  robusta.classList.add("hide");
+}
+const liberica = document.getElementById("liberica");
+function openLiberica() {
+  liberica.classList.remove("hide");
+}
+
+function closeLiberica() {
+  liberica.classList.add("hide");
+}
+const excelsa = document.getElementById("excelsa");
+function openExcelsa() {
+  excelsa.classList.remove("hide");
+}
+
+function closeExcelsa() {
+  excelsa.classList.add("hide");
+}
